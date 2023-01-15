67e0c734e4884df486cd37d9dc62ab18c85cfd5d
zeeshan <skzeeshan3650@gmail.com>
2023-01-15 22:51:48 +0530

    test

--- app/src/main/java/com/reiserx/tennis/Activities/MainActivity.java
+++ app/src/main/java/com/reiserx/tennis/Activities/MainActivity.java
@@ -1,44 +1,36 @@
 package com.reiserx.tennis.Activities;
 
-import androidx.annotation.NonUiContext;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Base64;
 import android.util.Log;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.canhub.cropper.CropImageContract;
+import com.canhub.cropper.CropImageContractOptions;
+import com.canhub.cropper.CropImageOptions;
 import com.reiserx.tennis.OCR.requestOcr;
-import com.reiserx.tennis.Utils.FileUtil;
 import com.reiserx.tennis.databinding.ActivityMainBinding;
-
-import org.checkerframework.common.value.qual.StringVal;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-
-import io.opencensus.internal.Utils;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
 
-    private static final int CAMERA_REQUEST = 1888;
-    private static final int MY_CAMERA_PERMISSION_CODE = 100;
-
     private requestOcr requestOcr;
     String TAG = "ihfsidhfs";
-    ImageView image;
-    Uri selectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,29 +38,61 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        requestOcr = new requestOcr();
-//            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-//            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        binding.button.setText("scan");
+        requestOcr = new requestOcr(this, binding.editTextTextPersonName, binding.output, binding.outputHolder);
+        binding.outputHolder.setVisibility(View.GONE);
+
+        binding.editTextTextPersonName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (binding.editTextTextPersonName.getText().toString().equals("")) {
+                    binding.button.setText("scan");
+                } else {
+                    binding.button.setText("run");
+                }
+            }
+        });
+
+        binding.button.setOnClickListener(view -> {
+            if (binding.button.getText().toString().equals("scan")) {
+                CropImageOptions cropImageOptions = new CropImageOptions();
+                cropImageOptions.imageSourceIncludeGallery = true;
+                cropImageOptions.imageSourceIncludeCamera = true;
+                CropImageContractOptions cropImageContractOptions = new CropImageContractOptions(null, cropImageOptions);
+                cropImage.launch(cropImageContractOptions);
+            } else {
+                try {
+                    requestOcr.runCode(binding.editTextTextPersonName.getText().toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    binding.output.setText(e.toString());
+                }
+            }
+        });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK && data != null)
-        {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            Log.d(TAG, String.valueOf(getEncoded64ImageStringFromBitmap(photo)));
-        } else {
-            Log.d(TAG, "data not found");
+    ActivityResultLauncher<CropImageContractOptions> cropImage = registerForActivityResult(new CropImageContract(), result -> {
+        if (result.isSuccessful()) {
+            Bitmap cropped = BitmapFactory.decodeFile(result.getUriFilePath(getApplicationContext(), true));
+            try {
+                requestOcr.scan_file(getEncoded64ImageStringFromBitmap(cropped));
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+            }
         }
-    }
+    });
 
-    public Uri getImageUri(Context inContext, Bitmap inImage) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
-    }
     public String getEncoded64ImageStringFromBitmap(Bitmap bitmap) {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
@@ -78,5 +102,22 @@ public class MainActivity extends AppCompatActivity {
         String imgString = Base64.encodeToString(byteFormat, Base64.NO_WRAP);
 
         return imgString;
+    }
+
+    public String loadJSONFile() {
+        String json = null;
+        try {
+            InputStream inputStream = getAssets().open("response.json");
+            int size = inputStream.available();
+            byte[] byteArray = new byte[size];
+            inputStream.read(byteArray);
+            inputStream.close();
+            json = new String(byteArray, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d(TAG, e.toString());
+            return null;
+        }
+        return json;
     }
 }
--- app/src/main/java/com/reiserx/tennis/OCR/requestOcr.java
+++ app/src/main/java/com/reiserx/tennis/OCR/requestOcr.java
@@ -1,28 +1,21 @@
 package com.reiserx.tennis.OCR;
 
-import android.graphics.Bitmap;
-import android.net.Uri;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.cloud.vision.v1.AnnotateImageRequest;
-import com.google.cloud.vision.v1.AnnotateImageResponse;
-import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
-import com.google.cloud.vision.v1.Feature;
-import com.google.cloud.vision.v1.Image;
-import com.google.cloud.vision.v1.ImageAnnotatorClient;
-import com.google.cloud.vision.v1.TextAnnotation;
-import com.google.protobuf.ByteString;
-import com.reiserx.tennis.R;
+import com.reiserx.tennis.Utils.FileUtil;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -34,29 +27,137 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class requestOcr {
+    Context context;
+    ProgressDialog prog;
+    EditText text;
+    TextView output;
+    View outputHolder;
     String TAG = "ihfsidhfs";
+    String result;
+    JSONObject jsonObject;
 
-    public void detectDocumentText(String bitmap) throws IOException {
+
+    public requestOcr(Context context, EditText text, TextView output, View outputHolder) {
+        this.context = context;
+        this.prog = new ProgressDialog(context);
+        this.text = text;
+        this.output = output;
+        this.outputHolder = outputHolder;
+    }
+
+    public void runCode(String code) throws IOException {
+        prog.setMessage("Sending request...");
+        prog.show();
         ExecutorService executor = Executors.newSingleThreadExecutor();
         Handler handler = new Handler(Looper.getMainLooper());
         executor.execute(() -> {
             try {
                 OkHttpClient client = new OkHttpClient();
 
-                JSONObject json = new JSONObject();
-                json.put("image", bitmap);
+                JSONObject request = new JSONObject();
+                request.put("code", code);
 
-                RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json.toString());
-                Request request = new Request.Builder()
-                        .url("http://reiserx.com/ocr/")
+                RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), request.toString());
+                Request apiRequest = new Request.Builder()
+                        .header("Authorization", "ihiuHUGUIjbijhygyg&YGGybihgy")
+                        .url("http://reiserx.com/python/api/")
                         .post(body)
                         .build();
-                Response response = client.newCall(request).execute();
-                String res = Objects.requireNonNull(response.body()).string();
-                Log.d(TAG, res);
+                Response response = client.newCall(apiRequest).execute();
+                result = Objects.requireNonNull(response.body()).string();
+                jsonObject = new JSONObject(result);
+                Log.d(TAG, result);
+                prog.dismiss();
             } catch (Exception e) {
-                Log.i("msgError", e.getMessage());
+                prog.dismiss();
+                Log.i(TAG, e.getMessage());
             }
+
+            prog.setMessage("Request sent");
+            prog.dismiss();
+
+            handler.post(() -> {
+                try {
+                    if (jsonObject.getInt("success") == 1) {
+                        output.setText(jsonObject.getString("response"));
+                    } else {
+                        output.setText("Error code 0: ".concat(jsonObject.getString("response")));
+                    }
+                    if (outputHolder.getVisibility() == View.GONE)
+                        outputHolder.setVisibility(View.VISIBLE);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.d(TAG, e.toString());
+                }
+            });
+        });
+    }
+
+    public String prints(String res) throws JSONException {
+        JSONObject json = new JSONObject(res);
+        JSONArray jsonObject = json.getJSONArray("responses");
+        JSONObject object = jsonObject.getJSONObject(0);
+        JSONArray data = object.getJSONArray("textAnnotations");
+        JSONObject object1 = data.getJSONObject(0);
+        return object1.getString("description");
+    }
+
+    public String testing(String result) {
+        return result;
+    }
+
+    public void write(String path, String data) throws IOException {
+        FileUtil.writeFile(path, data);
+        Log.d(TAG, "file written");
+        Log.d(TAG, data);
+    }
+
+    public void scan_file(String uri) throws IOException {
+        prog.setMessage("Sending request...");
+        prog.show();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        executor.execute(() -> {
+            try {
+                OkHttpClient client = new OkHttpClient();
+
+                JSONObject request = new JSONObject();
+
+                request.put("image", uri);
+
+                RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), request.toString());
+                Request apiRequest = new Request.Builder()
+                        .header("Authorization", "ihiuHUGUIjbijhygyg&YGGybihgy")
+                        .url("http://reiserx.com/python/scan/")
+                        .post(body)
+                        .build();
+                Response response = client.newCall(apiRequest).execute();
+                result = Objects.requireNonNull(response.body()).string();
+
+                jsonObject = new JSONObject(result);
+
+                prog.dismiss();
+            } catch (Exception e) {
+                prog.dismiss();
+                Log.i(TAG, e.getMessage());
+            }
+
+            prog.setMessage("Request sent");
+            prog.dismiss();
+
+            handler.post(() -> {
+                try {
+                    if (jsonObject.getInt("success") == 1) {
+                        text.setText(jsonObject.getString("response"));
+                    } else {
+                        output.setText("Error code 0: ".concat(jsonObject.getString("response")));
+                    }
+                    if (outputHolder.getVisibility() == View.GONE)
+                        outputHolder.setVisibility(View.VISIBLE);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            });
         });
     }
 }
